a9b3f468dcba2b82afa3aaeedced4078761cfdfe
Md-arefin <ansariarefin617@gmail.com>
2023-05-20 19:23:02 +0600

    implemented add toy page

--- src/pages/AllToys/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys/AllToys.jsx
@@ -1,10 +1,26 @@
-
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 const AllToys = () => {
     return (
         <div>
-            <h3 className="text-center my-10 text-2xl font-semibold text-white lg:text-3xl">Enhance Your Gaming Setup: Elite Accessories for the Ultimate Experience</h3>
-            
+            <div>
+                <h3 className="text-center my-10 text-2xl font-semibold text-white lg:text-3xl">Enhance Your Gaming Setup: Elite Accessories for the Ultimate Experience</h3>
+
+                {/*  */}
+
+                <div>
+                    <Player
+                        autoplay
+                        loop
+                        src="https://assets10.lottiefiles.com/packages/lf20_o5dfcnmb.json"
+                        className='lg:w-[75%]'
+                    >
+                        <Controls visible={!true} buttons={['play', 'repeat', 'frame', 'debug']} />
+                    </Player>
+                </div>
+
+            </div>
+
         </div>
     );
 };
--- src/pages/Home/Countdown/Countdown.jsx
+++ src/pages/Home/Countdown/Countdown.jsx
@@ -1,50 +1,53 @@
 import { useState, useEffect } from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+
 
 const Countdown = () => {
 
 
     const [countdownDate] = useState(new Date("jul 5, 2023 15:37:25").getTime());
-  const [timeRemaining, setTimeRemaining] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
+    const [timeRemaining, setTimeRemaining] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    });
 
-  useEffect(() => {
-    const updateTimer = () => {
-      const now = new Date().getTime();
-      const distance = countdownDate - now;
+    useEffect(() => {
+        const updateTimer = () => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
 
-      if (distance < 0) {
-        // Countdown is over
-        clearInterval(interval);
-      } else {
-        // Calculate remaining time
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            if (distance < 0) {
+                // Countdown is over
+                clearInterval(interval);
+            } else {
+                // Calculate remaining time
+                const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        setTimeRemaining({ days, hours, minutes, seconds });
-      }
-    };
+                setTimeRemaining({ days, hours, minutes, seconds });
+            }
+        };
 
-    const interval = setInterval(updateTimer, 1000);
+        const interval = setInterval(updateTimer, 1000);
 
-    return () => clearInterval(interval);
-  }, [countdownDate]);
+        return () => clearInterval(interval);
+    }, [countdownDate]);
 
     return (
 
 
-        <div className='animate-bounce w-[90%] mx-auto'>
+        <div className='animate-bounce w-[90%] mx-auto relative '>
             <div className="lg:h-48 stats bg-white text-black shadow w-full text-center">
 
-                <div className="stat relative ">
-                    <div className="stat-title font-semibold lg:text-3xl text-black ">Level Up Your Summer Gaming: 
-                    <span  className='lg:hidden'> <br/></span> 
-                     Unbeatable Deals on Elite Gear!</div>
+                <div className="stat ">
+                    <div className="stat-title font-semibold lg:text-3xl text-black ">Level Up Your Summer Gaming:
+                        <span className='lg:hidden'> <br /></span>
+                        Unbeatable Deals on Elite Gear!</div>
 
                     <div className="grid grid-flow-col gap-5 text-center auto-cols-max ml-10 lg:ml-96 mt-3">
                         <div className="flex flex-col">
@@ -72,10 +75,18 @@ const Countdown = () => {
                             sec
                         </div>
                     </div>
-                    <div className="mt-5 absolute lg:top-10 lg:left-[210px]
-                    top-14 
+                    <div className="mt-5 absolute top-[70px] lg:top-[-113px] lg:right-[700px]
                     ">
-                        <img className='w-[25%] lg:w-[30%]' src="https://i.ibb.co/zrBQCmw/507-5073772-30-off-sticker-png-transparent-png-removebg-preview.png" alt="" />
+                        <div>
+                            <Player
+                                autoplay
+                                loop
+                                src="https://assets3.lottiefiles.com/packages/lf20_zsrpgica.json"
+                                className='w-[50%] lg:w-[75%] '
+                            >
+                                <Controls visible={!true} buttons={['play', 'repeat', 'frame', 'debug']} />
+                            </Player>
+                        </div>
                     </div>
                 </div>
 
